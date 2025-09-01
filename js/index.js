7e492ce191494438dfe33682845912da94df50fe
Elina4224 <elina.huseynova.codes@gmail.com>
2025-08-31 21:11:13 -0500

    Update Java Script and CSS

--- js/index.js
+++ js/index.js
@@ -1,30 +1,45 @@
 function updateTime() {
-  let losAngelesElement = document.querySelector("#los-angeles");
-  if (losAngelesElement) {
-    let losAngelesDateElement = losAngelesElement.querySelector(".date");
-    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-    let losAngelesTime = moment().tz("America/Los_Angeles");
+  let houstonElement = document.querySelector("#houston");
+  if (houstonElement) {
+    let houstonDateElement = houstonElement.querySelector(".date");
+    let houstonTimeElement = houstonElement.querySelector(".time");
+    let houstonTime = moment().tz("America/Houston");
 
-    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    houstonDateElement.innerHTML = houstonTime.format("MMMM Do YYYY");
+    houstonTimeElement.innerHTML = houstonTime.format(
       "h:mm:ss [<small>]A[</small>]"
     );
   }
 
-  let parisElement = document.querySelector("#paris");
-  if (parisElement) {
-    let parisDateElement = parisElement.querySelector(".date");
-    let parisTimeElement = parisElement.querySelector(".time");
-    let parisTime = moment().tz("Europe/Paris");
+  let bakuElement = document.querySelector("#baku");
+  if (bakuElement) {
+    let bakuDateElement = bakuElement.querySelector(".date");
+    let bakuTimeElement = bakuElement.querySelector(".time");
+    let bakuTime = moment().tz("Asia/Baku");
 
-    parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-    parisTimeElement.innerHTML = parisTime.format(
+    bakuDateElement.innerHTML = bakuTime.format("MMMM Do YYYY");
+    bakuTimeElement.innerHTML = bakuTime.format("h:mm:ss [<small>]A[</small>]");
+  }
+
+  let madridElement = document.querySelector("#madrid");
+  if (madridElement) {
+    let madridDateElement = madridElement.querySelector(".date");
+    let madridTimeElement = madridElement.querySelector(".time");
+    let madridTime = moment().tz("Europe/Madrid");
+
+    madridDateElement.innerHTML = madridTime.format("MMMM Do YYYY");
+    madridTimeElement.innerHTML = madridTime.format(
       "h:mm:ss [<small>]A[</small>]"
     );
   }
 }
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
@@ -32,10 +47,11 @@ function updateCity(event) {
         <div><h2>${cityName}</h2>
         <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
-        
-        <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
-    "A"
-  )}</small></div>
+  
+        <div class="time">${cityTime.format(
+          "h:mm:ss"
+        )} <small>${cityTime.format("A")}</small></div>
+
     </div>`;
 }
 
